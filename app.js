112f1587543e90ec6957f229843ab7e2d95706dc
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-06-22 23:22:01 -0500

    added output folder and updated readme

--- app.js
+++ app.js
@@ -10,90 +10,145 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+
+// Prompt user for roles to enter 
+function employeeInfo() {
+   inquirer.prompt([
+    {
+      type: "list",
+      message: "What type of employee would you like to input",
+      name: "name",
+      choices: ["Manager", "Engineer", "Intern", "Show Summary"],
+    },
+  ]).then(val => {
+    if (val.name === "Manager") {
+      managerInfo();
+    } else if (val.name === "Engineer") {
+      engineerInfo()
+    } else if (val.name === "Intern") {
+      internInfo();
+    } else if (val.name === "Show Summary") {
+      generateHTML(outputPath, render(team));
+    };
+  }); 
+}; // end of function 
+
+// Prompt to collect information on manager and then go back to enter a new employee
+function managerInfo() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "what is your manager's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your manager's id",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your manager's email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your manager's office number",
+      name: "number",
+    },
+  ]).then(function(answer) {
+    let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
+    team.push(manager);
+
+    employeeInfo()
+  })
+}; // end of function 
+
+
+// Prompt to collect information on engineer and then go back to enter a new employee
+function engineerInfo() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "what is your engineer's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your engineer's ID",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your engineer's email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your engineer's GitHub username",
+      name: "GitHub",
+    },
+  ]).then(function(answer) {
+    let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
+    team.push(engineer);
+
+    employeeInfo()
+  })
+}; // end of function 
+
+
+// Prompt to collect information on intern and then go back to enter a new employee
+function internInfo() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "what is your intern's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is your intern's ID",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your intern's email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is your intern's school",
+      name: "school",
+    },
+  ]).then(function(answer) {
+    let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+    team.push(intern);
+
+    employeeInfo()
+  })
+}; // end of function 
+
+
+
+// write the answers to a new README file
+function generateHTML(fileName, data) {
+  fs.writeFile(fileName, data, "utf8", function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("You have successfully written your Employee Summary");
+  });
+};
+
+
+// Calls function to begin prompt for roles to enter
+employeeInfo();
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const team = [];
 
-function employeeInfo() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'Sellect employee type:',
-            name: 'name',
-            choices: ['Manager', 'Engineer', 'Intern', 'Show Summary'],
-        },
-    ]).then(val => {
-        if (val.name === 'Manager') {
-            managerInfo();
-        } else if (val.name === 'Engineer') {
-            engineerInfo()
-        } else if (val.name === 'Intern') {
-            internInfo();
-        } else if (val.name === 'Show Summary') {
-            generateHTML(outputPath, render(team));
-        };
-    });
-};
-
-function managerInfo() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            message: "What is your manager's name?",
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: "What is your manager's id?",
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: "What is your manager's email?",
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message: "What is your manager's office number?",
-            name: 'number'
-        },
-    ]).then(function(answer) {
-        let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
-        team.push(manager);
-
-        employeeInfo();
-    });
-};
-
-function engineerInfo() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "what is your engineer's name?",
-            name: "name"
-        },
-        {
-            type: 'input',
-            message: "What is your engineer's ID?",
-        },
-        {
-            type: "input",
-            message: "What is your engineer's email?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is your engineer's GitHub username",
-            name: "GitHub",
-        },
-    ]).then(function(answer) {
-        let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
-        team.push(engineer);
-
-        employeeInfo();
-    });
-};
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
