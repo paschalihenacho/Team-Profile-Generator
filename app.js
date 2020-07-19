3abde68cae60aa73c501a59692704bb6262ed23c
Paschal Ihenacho <ihenachopaschal@yahoo.com>
2020-07-19 13:02:07 -0500

    implemented cases for Engineer and Intern

--- app.js
+++ app.js
@@ -13,82 +13,103 @@ const render = require("./lib/htmlRenderer");
 const employees = [];
 
 async function repeat() {
-	await inquirer
-		.prompt({
-			type: "confirm",
-			name: "repeat",
-			message: "Want to add another employee?",
-			default: true,
-		})
-		.then(async (res) => {
-			if (res.repeat) {
-				await employee();
-			} else {
-				console.log("generating team page!");
-			}
-		});
+  await inquirer
+    .prompt({
+      type: "confirm",
+      name: "repeat",
+      message: "Want to add another employee?",
+      default: true,
+    })
+    .then(async (res) => {
+      if (res.repeat) {
+        await employee();
+      } else {
+        console.log("generating team page!");
+      }
+    });
 }
 
 async function employee() {
-	await inquirer
-		.prompt([
-			{
-				type: "list",
-				name: "role",
-				message: "What is your role?",
-				choices: ["Manager", "Engineer", "Intern"],
-			},
-			{
-				type: "input",
-				name: "name",
-				message: "What is your name?",
-			},
-			{
-				type: "input",
-				name: "email",
-				message: "What is your email?",
-			},
-			{
-				type: "input",
-				name: "id",
-				message: "What is your id?",
-			},
-		])
-		.then(async (res) => {
-			switch (res.role) {
-				case "Manager":
-					await inquirer
-						.prompt({
-							type: "input",
-							name: "office",
-							message: "What is your office number?",
-						})
-						.then((office) => {
-							employees.push(
-								new Manager(
-									res.name,
-									res.id,
-									res.email,
-									office.office
-								)
-							);
-						});
-					break;
-			}
+  await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is your role?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your id?",
+      },
+    ])
+    .then(async (res) => {
+      switch (res.role) {
+        case "Manager":
+          await inquirer
+            .prompt({
+              type: "input",
+              name: "office",
+              message: "What is your office number?",
+            })
+            .then((office) => {
+              employees.push(
+                new Manager(res.name, res.id, res.email, office.office)
+              );
+            });
+          break;
+        case "Engineer":
+          await inquirer
+            .prompt({
+              type: "input",
+              name: "github",
+              message: "What is your GitHub?",
+            })
+            .then((github) => {
+              employees.push(
+                new Engineer(res.name, res.id, res.email, github.github)
+              );
+            });
+          break;
+        case "Intern":
+          await inquirer
+            .prompt({
+              type: "input",
+              name: "school",
+              message: "What is your School?",
+            })
+            .then((school) => {
+              employees.push(
+                new Intern(res.name, res.id, res.email, school.school)
+              );
+            });
+          break;
+      }
     });
-    await repeat() 
+  await repeat();
 }
 
 async function init() {
   await employee().then(() => {
-      const html = render(employees);
-      if (!fs.existsSync(OUTPUT_DIR)) {
-          fs.mkdirSync(OUTPUT_DIR);
-      }
-      fs.writeFileSync(outputPath, html, err => {
-          if (err) throw err;
-          console.log("file successfully written")
-      })
-  })
+    const html = render(employees);
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, html, (err) => {
+      if (err) throw err;
+      console.log("file successfully written");
+    });
+  });
 }
 init();
